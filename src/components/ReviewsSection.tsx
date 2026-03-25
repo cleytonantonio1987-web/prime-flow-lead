ab5f9abbd0a663582df7524dda3f5fc905704eb1
cleytonantonio1987-web <cleyton.antonio1987@gmail.com>
2026-03-25 15:55:03 -0300

    Update ReviewsSection.tsx

--- src/components/ReviewsSection.tsx
+++ src/components/ReviewsSection.tsx
@@ -2,18 +2,13 @@
 <html lang="pt-BR">
   <head>
     <meta charset="UTF-8" />
-    <script>
-      (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-      })(window,document,'script','dataLayer','GTM-K5VCKTST');
-    </script>
     <link rel="icon" type="image/png" sizes="40x40" href="/favicon.png?v=30">
     <link rel="icon" type="image/png" sizes="16x16" href="/favicon.png?v=30">
     <link rel="shortcut icon" href="/favicon.png?v=30">
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Prime Pisos e Revestimentos | Pisos Premium em Curitiba</title>
+    
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-K5VCKTST');</script>
     <meta name="description" content="Pisos laminados, vinílicos, porcelanatos e revestimentos em Curitiba. Atendimento especializado e instalação profissional. Solicite seu orçamento pelo WhatsApp.">
     <meta name="robots" content="index, follow" />
     <meta property="og:type" content="website" />
